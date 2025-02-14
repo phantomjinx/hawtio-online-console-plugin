26d8d5246453c6323f42ae513d33a31db9365394
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2025-02-14 21:02:02 +0000

    Replace the plugin dropdown with a select component
    * Allows the display of the selected plugin title in the select value on
      the header bar.
    
    * Populate the select value on loading of the PluginMenuDropdown component

--- packages/plugin/src/plugins/PluginMenuDropdown.tsx
+++ packages/plugin/src/plugins/PluginMenuDropdown.tsx
@@ -1,66 +1,99 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
-  Dropdown,
-  DropdownItem,
-  DropdownList,
   MenuToggle,
   MenuToggleElement,
+  Select,
+  SelectList,
+  SelectOption,
 } from '@patternfly/react-core'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import { ModuleIcon } from '@patternfly/react-icons'
-import { hawtio, usePlugins } from '@hawtio/react'
+import { hawtio, usePlugins, Plugin } from '@hawtio/react'
 import { log } from '../globals'
 
 export const PluginMenuDropDown: React.FunctionComponent = () => {
   const { plugins, pluginsLoaded } = usePlugins()
+  const location = useLocation()
   const [isOpen, setIsOpen] = React.useState<boolean>(false)
+  const [selected, setSelected] = React.useState<string>('')
+
+  const pluginEntry = (plugin: Plugin) => {
+    return plugin.title ? plugin.title : plugin.id
+  }
+
+  /*
+   * On resolving of all plugins
+   * set the current plugin in the selected property
+   */
+  useEffect(() => {
+    const filteredPlugins = plugins.filter(plugin => plugin.path != null)
+    const activePlugins = filteredPlugins
+      .filter(plugin => {
+        let pluginPath = hawtio.fullPath(plugin.path!)
+
+        if (!pluginPath.startsWith('/'))
+          pluginPath = '/' + pluginPath
+
+        return location.pathname.startsWith(pluginPath)
+      })
+
+    const activePlugin = activePlugins.length > 0 ? activePlugins[0] :
+      (filteredPlugins.length > 0) ? filteredPlugins[0] : null
+
+    setSelected(activePlugin ? pluginEntry(activePlugin) : '')
+  }, [plugins])
+
+  const onToggleClick = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const onSelect = (_event: React.MouseEvent<Element, MouseEvent> | undefined, value: string | number | undefined) => {
+    setSelected(value as string)
+    setIsOpen(false)
+  }
 
   if (!pluginsLoaded) {
     log.debug('Loading:', 'plugins =', pluginsLoaded)
     return <></>
   }
 
-  const onFocus = () => {
-    const element = document.getElementById('toggle-basic')
-    element?.focus()
-  }
-
-  const onSelect = () => {
-    setIsOpen(false)
-    onFocus()
-  }
-
-  const filteredPlugins = plugins.filter(plugin => plugin.path != null)
   return (
-    <Dropdown
+    <Select
+      id="single-select"
       className='online-header-toolbar-dropdown'
-      onSelect={onSelect}
-      toggle={(toggleRef: React.Ref<MenuToggleElement>) => (
-        <MenuToggle
-          id='plugin-header-dropdown-toggle'
-          className='plugin-header-dropdown-toggle'
-          variant={'plain'}
-          ref={toggleRef}
-          onClick={() => setIsOpen(!isOpen)}
-          isExpanded={isOpen}
-        >
-          <ModuleIcon />
-        </MenuToggle>
-      )}
       isOpen={isOpen}
-      onOpenChange={setIsOpen}
-    >
-      <DropdownList>
-        {filteredPlugins.map(plugin => (
-          <DropdownItem
-            value={1}
-            key={plugin.id}
-            component={(props: any) => <NavLink {...props} to={hawtio.fullPath(plugin.path!)} /> }
+      selected={selected}
+      onSelect={onSelect}
+      onOpenChange={(isOpen) => setIsOpen(isOpen)}
+      shouldFocusToggleOnSelect
+      toggle={(toggleRef: React.Ref<MenuToggleElement>) => (
+          <MenuToggle
+            id='plugin-header-dropdown-toggle'
+            className='plugin-header-dropdown-toggle'
+            variant={'plain'}
+            ref={toggleRef}
+            onClick={onToggleClick}
+            isExpanded={isOpen}
           >
-            {plugin.title}
-          </DropdownItem>
-        ))}
-      </DropdownList>
-    </Dropdown>
+            <ModuleIcon /> {selected}
+          </MenuToggle>
+      )}
+    >
+      <SelectList>
+        {plugins
+          .filter(plugin => plugin.path != null)
+          .map(plugin => (
+            <SelectOption
+              value={pluginEntry(plugin)}
+              key={plugin.id}
+              isSelected={selected === pluginEntry(plugin)}
+              component={props => <NavLink {...props} to={hawtio.fullPath(plugin.path!)} /> }
+            >
+              {pluginEntry(plugin)}
+            </SelectOption>
+          ))
+        }
+      </SelectList>
+    </Select>
   )
 }
